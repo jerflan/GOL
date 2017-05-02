47ef3a3ea862c5985bc70b30e7d9261ec0fd9888
Jeremy Flanagan <jeremy.flanagan@instacart.com>
2017-05-01 22:40:25 -0700

    Wrote code for creating grid, colony, assigning 'alive' or 'dead', and getting alive_count

--- game_of_life_specs.rb
+++ game_of_life_specs.rb
@@ -1,20 +1,91 @@
-ma = [
-  [1,2,3,4,5],      #[0] = row #1
-  [6,7,8,9,10],     #[1] = row #2
-  [11,12,13,14,15], #[2] = row #3
-  [16,17,18,19,20], #[3] = row #4
-  [21,22,23,24,25]  #[4] = row #5
+grid = [
+  [0,0,0,0,0],      #[0] = row #1
+  [0,0,0,0,0],     #[1] = row #2
+  [0,0,0,0,0], #[2] = row #3
+  [0,0,0,0,0], #[3] = row #4
+  [0,0,0,0,0]  #[4] = row #5
 ]
 
-multi_array.each do |x|
+puts "Grid:"
+grid.each do |x|
   print "#{x}\n"
 end
 
-print multi_array[0]
-puts "\n"
-print multi_array[0][2]
-puts "\n"
-print
+puts "-" * 40
+
+grid[0][0] = "a" # defining which cells are alive
+grid[1][1] = "a"
+
+colony = [grid[0][0..2],grid[1][0..2], grid[2][0..2]]
+puts "Colony:"
+colony.each do |x|
+  print "#{x}\n"
+end
+
+puts "-" * 40
+
+primary_cell = grid[1][1]
+puts "Primary Cell: = grid[1][1]"
+
+puts "-" * 40
+
+puts "How many cells in the colony are alive?"
+
+def alive_count(colony)
+  alive = 0
+  colony.each_index do  |x| # assessing the value of each cell
+    colony[x].each_index do |y|
+      if colony[x][y] == "a"
+        puts "[#{x}][#{y}] == #{colony[x][y]} is true"
+        alive += 1
+      else
+        puts false
+      end
+    end
+  end
+puts "Number of Alive Cells: #{alive}"
+end
+
+alive_count(colony)
+puts alive
+
+puts "-" * 40
+
+
+
+puts "-" * 40
+
+grid.each_index do |x|
+  if x < 3
+    grid[x].each_index do |y|
+      if y < 3
+        puts grid[x][y] #calls the value of the coordinate in the colony
+      else
+        break
+      end
+    end
+  else
+    break
+  end
+end
+
+
+# ma = [
+#   [1,2,3,4,5],      #[0] = row #1
+#   [6,7,8,9,10],     #[1] = row #2
+#   [11,12,13,14,15], #[2] = row #3
+#   [16,17,18,19,20], #[3] = row #4
+#   [21,22,23,24,25]  #[4] = row #5
+# ]
+
+# multi_array.each do |x|
+#   print "#{x}\n"
+# end
+
+# print multi_array[0]
+# puts "\n"
+# print multi_array[0][2]
+# puts "\n"
 
 
 
